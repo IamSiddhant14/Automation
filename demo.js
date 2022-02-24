3cf8282ba9d14189422343890b50179b958f9fb7
Siddhant <siddhantsharma669@gmail.com>
2022-02-25 01:30:55 +0530

    Able to open a new tab in chromium , Added screen shot for refrence

---
+++ demo.js
@@ -0,0 +1,36 @@
+let puppeteer = require('puppeteer');
+
+//This is an promised based function of puppeteer which will return promise, currenlty which is in pending stage when resolved returns browser instance
+
+console.log( "Before");
+
+//****************************REFFER THE SCREENSHOT TO UNDESTAND CHANING */
+let browserWillbeLauncedPromise = puppeteer.launch({
+    headless: false, //changing the headless prop to false as of which it Unhides all the activities performed by puppeteer
+    defaultViewport:null,
+});
+
+browserWillbeLauncedPromise.then(function( browserInstance ){ //When the promise returned by puppeteer.launch get resolved then it will be handled by "then"
+
+    // This will lauch chromium
+
+    // return browserInstance; Browser instance Or simply chrome
+
+    // To open multiple tabs using automation in chromium
+    
+    let newTabPromise = browserInstance.newPage();//This will open a new tab/page
+    return newTabPromise;
+
+}).then(function(newTab){
+    
+    console.log('New Tab Opened')
+    let pageWillbeOpenedPromise = newTab.goto('https://www.pepcoding.com/');
+    return pageWillbeOpenedPromise
+
+}).then( function(webPage){
+    console.log('Website opened');
+})
+
+console.log("After");
+
+
